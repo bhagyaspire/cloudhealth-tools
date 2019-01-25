7224975f0ef22759e99885ad5e7be82fb292ce76
NimbusScale <joe@nimbusscale.com>
2019-01-24 17:27:07 -0800

    test delete client

--- tests/test_aws_account_client.py
+++ tests/test_aws_account_client.py
@@ -1,33 +1,47 @@
 import requests_mock
 
+import pytest
+
 from chtools.aws_account.client import AwsAccountClient
 
 
-def test_create():
+@pytest.fixture()
+def mock_schema_response():
+    return {
+        'id': 5909874999458, 
+        'name': 'bcttest1',
+        'amazon_name': 'bcttest1', 'owner_id': '619288149268',
+        'hide_public_fields': False, 'region': 'global',
+        'created_at': '2018-09-22T15:51:04Z',
+        'updated_at': '2019-01-19T22:33:39Z',
+        'account_type': 'Standalone', 'vpc_only': True,
+        'cluster_name': 'bcttest1',
+        'status': {'level': 'yellow',
+                  'last_update': '2019-01-20T17:43:30Z'},
+        'authentication': {
+           'protocol': 'assume_role',
+            'assume_role_arn': 'arn:aws:iam::619288149268:role/CloudHealth',
+            'assume_role_external_id': '27beaf0b252df5700240e09ea7e96d'
+        },
+        'billing': {'is_consolidated': False},
+        'cloudtrail': {'enabled': False}, 'ecs': {'enabled': False},
+        'aws_config': {'enabled': False},
+        'cloudwatch': {'enabled': True}, 'tags': [],
+        'groups': [{'name': 'filter_test', 'group': 'Other'}],
+        '_links': {
+           'self': {'href': '/v1/aws_accounts/5909874999458'}}
+    }
+
+
+def test_create(mock_schema_response):
     client = AwsAccountClient('fake_api_key')
 
     mock_generated_external_id_response = {
         'generated_external_id': '27beaf0b252df5700240e09ea7e96d'
     }
 
-    mock_response_schema = {'id': 5909875000670, 'name': 'test_create',
-                            'hide_public_fields': False, 'region': 'global',
-                            'created_at': '2019-01-25T01:06:25Z',
-                            'updated_at': '2019-01-25T01:06:25Z',
-                            'account_type': 'Unknown',
-                            'status': {'level': 'unknown'},
-                            'authentication': {'protocol': 'assume_role',
-                                               'assume_role_arn': 'arn:aws:iam::619288149268:role/CloudHealth',
-                                               'assume_role_external_id': '27beaf0b252df5700240e09ea7e96d'},
-                            'billing': {'bucket': 'ch-dbr-bucket',
-                                        'is_consolidated': False},
-                            'cloudtrail': {'enabled': False},
-                            'ecs': {'enabled': False},
-                            'aws_config': {'enabled': False},
-                            'cloudwatch': {'enabled': True}, 'tags': []}
-
     account_schema = {
-        'name': 'test_create',
+        'name': 'bcttest1',
         'authentication': {
             'protocol': 'assume_role',
             'assume_role_arn': 'arn:aws:iam::619288149268:role/CloudHealth'
@@ -44,57 +58,54 @@ def test_create():
         )
         m.post(
             'https://chapi.cloudhealthtech.com/v1/aws_accounts',
-            json=mock_response_schema
+            json=mock_schema_response
         )
 
         results = client.create(account_schema)
 
-    assert results.id == 5909875000670
+    assert results.id == 5909874999458
 
 
-def test_get_spec_by_account_id():
+def test_delete(mock_schema_response):
     client = AwsAccountClient('fake_api_key')
-
-    mock_schema = {'id': 5909874999458, 'name': 'bctlinktest1',
-                   'amazon_name': 'bctlinktest1', 'owner_id': '619288149268',
-                   'hide_public_fields': False, 'region': 'global',
-                   'created_at': '2018-09-22T15:51:04Z',
-                   'updated_at': '2019-01-19T22:33:39Z',
-                   'account_type': 'Standalone', 'vpc_only': True,
-                   'cluster_name': 'bctlinktest1',
-                   'status': {'level': 'yellow',
-                              'last_update': '2019-01-20T17:43:30Z'},
-                   'authentication': {'protocol': 'assume_role',
-                                      'assume_role_arn': 'arn:aws:iam::619288149268:role/CloudHealth',
-                                      'assume_role_external_id': '27beaf0b252df5700240e09ea7e96d'},
-                   'billing': {'is_consolidated': False},
-                   'cloudtrail': {'enabled': False}, 'ecs': {'enabled': False},
-                   'aws_config': {'enabled': False},
-                   'cloudwatch': {'enabled': True}, 'tags': [],
-                   'groups': [{'name': 'filter_test', 'group': 'Other'}],
-                   '_links': {
-                       'self': {'href': '/v1/aws_accounts/5909874999458'}}}
 
     with requests_mock.Mocker() as m:
         m.get(
             'https://chapi.cloudhealthtech.com/v1/aws_accounts/5909874999458',
-            json=mock_schema
+            json=mock_schema_response
+        )
+        m.delete(
+            'https://chapi.cloudhealthtech.com/v1/aws_accounts/5909874999458'
+        )
+
+        results = client.delete('5909874999458')
+
+    assert results.id is None
+
+
+def test_get_spec_by_account_id(mock_schema_response):
+    client = AwsAccountClient('fake_api_key')
+
+    with requests_mock.Mocker() as m:
+        m.get(
+            'https://chapi.cloudhealthtech.com/v1/aws_accounts/5909874999458',
+            json=mock_schema_response
         )
         results = client.get_by_account_id('5909874999458')
 
-    assert results.name == 'bctlinktest1'
+    assert results.name == 'bcttest1'
 
 
 def test_get_spec_by_name():
     client = AwsAccountClient('fake_api_key')
 
     mock_list = {'aws_accounts': [
-        {'id': 5909874999458, 'name': 'bctlinktest1',
-         'amazon_name': 'bctlinktest1', 'owner_id': '619288149268',
+        {'id': 5909874999458, 'name': 'bcttest1',
+         'amazon_name': 'bcttest1', 'owner_id': '619288149268',
          'hide_public_fields': False, 'region': 'global',
          'created_at': '2018-09-22T15:51:04Z',
          'updated_at': '2019-01-19T22:33:39Z', 'account_type': 'Standalone',
-         'vpc_only': True, 'cluster_name': 'bctlinktest1',
+         'vpc_only': True, 'cluster_name': 'bcttest1',
          'status': {'level': 'yellow', 'last_update': '2019-01-20T17:43:30Z'},
          'authentication': {'protocol': 'assume_role',
                             'assume_role_arn': 'arn:aws:iam::619288149268:role/CloudHealth',
@@ -126,7 +137,7 @@ def test_get_spec_by_name():
             'https://chapi.cloudhealthtech.com/v1/aws_accounts/',
               json=mock_list
         )
-        results = client.get_by_name('bctlinktest1')
+        results = client.get_by_name('bcttest1')
 
     assert results.owner_id == '619288149268'
 
