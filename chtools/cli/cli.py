60f11c7b27c50246dc475851f244cf7237d8739c
NimbusScale <joe@nimbusscale.com>
2019-01-26 09:59:56 -0800

    change service to feature

--- chtools/cli/cli.py
+++ chtools/cli/cli.py
@@ -16,13 +16,13 @@ def parse_args(arguments):
         add_help=False
     )
 
-    parser.add_argument('service',
+    parser.add_argument('feature',
                         choices=[
                             'aws-account',
                             'perspective',
                             'help'
                         ],
-                        help='CloudHealth service that you wish to '
+                        help='CloudHealth feature that you wish to '
                              'interact with.')
     parser.add_argument('--api-key',
                         help="CloudHealth API Key. May also be set via the "
@@ -32,24 +32,24 @@ def parse_args(arguments):
     parser.add_argument('--log-level',
                         default='info',
                         help="Log level sent to the console.")
-    service_args, action_args = parser.parse_known_args(args=arguments)
+    feature_args, action_args = parser.parse_known_args(args=arguments)
 
-    if service_args.service == 'help':
+    if feature_args.feature == 'help':
         parser.print_help()
         sys.exit(0)
-    return service_args, action_args
+    return feature_args, action_args
 
 
-def service_to_handler(service_name):
+def feature_to_handler(feature_name):
     name_to_handler = {
         'aws-account': AwsAccountCliHandler,
         'perspective': PerspectiveCliHandler
     }
-    return name_to_handler[service_name]
+    return name_to_handler[feature_name]
 
 
 def main(arguments=sys.argv[1:]):
-    service_args, action_args = parse_args(arguments)
+    feature_args, action_args = parse_args(arguments)
 
     logging_levels = {
         'debug':    logging.DEBUG,
@@ -57,14 +57,14 @@ def main(arguments=sys.argv[1:]):
         'warn':     logging.WARN,
         'error':    logging.ERROR
     }
-    log_level = logging_levels[service_args.log_level.lower()]
+    log_level = logging_levels[feature_args.log_level.lower()]
 
     logger.setLevel(log_level)
     console_handler = logging.StreamHandler()
     logger.addHandler(console_handler)
 
-    if service_args.api_key:
-        api_key = service_args.api_key
+    if feature_args.api_key:
+        api_key = feature_args.api_key
     elif os.environ.get('CH_API_KEY'):
         api_key = os.environ['CH_API_KEY']
     else:
@@ -73,11 +73,11 @@ def main(arguments=sys.argv[1:]):
             "CH_API_KEY environment variable."
         )
 
-    cli_handler_class = service_to_handler(service_args.service)
+    cli_handler_class = feature_to_handler(feature_args.feature)
     cli_handler = cli_handler_class(
         action_args,
         api_key,
-        client_api_id=service_args.client_api_id,
+        client_api_id=feature_args.client_api_id,
         log_level=log_level
     )
     cli_handler.execute()
